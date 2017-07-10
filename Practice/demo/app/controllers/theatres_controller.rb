531f35324561a11580196bb8f487c563ee9e8fc1
arminderk <arminder.khinda@yahoo.com>
2017-07-10 16:40:20 -0700

    Theaters edit works, but not displaying parameters on edit form

--- Practice/demo/app/controllers/theatres_controller.rb
+++ Practice/demo/app/controllers/theatres_controller.rb
@@ -5,19 +5,48 @@ class TheatresController < ApplicationController
   def index
     @theaters = Theatre.order(:name)
   end
+  
+  def show
+    @theater = Theatre.find(params[:id])
+  end
 
   def new
+    @theater = Theatre.new
   end
   
   def create
-  end
-
-  def show
+    @theater = Theatre.new(theater_params)
+    if @theater.save
+      redirect_to(:action => 'index')
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @theater = Theatre.find(params[:id])
+  end
+  
+  def update
+    @theater = Theatre.find(params[:id])
+    if @theater.update_attributes(theater_params)
+      redirect_to(:action => 'show', :id => @theater.id)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @theater = Theatre.find(params[:id])
   end
+  
+  def destroy
+    theater = Theater.find(params[:id]).destroy
+    redirect_to(:action => 'index')
+  end
+  
+  private
+    def theater_params
+      params.require(:theatre).permit(:name, :price)
+    end
 end
