fca854baafec8ed1d146bc99ead8132df750593a
ArminderK <arminder.khinda@yahoo.com>
2017-09-03 11:05:37 -0700

    Comments part works now, have to add destroy in comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
+  
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      @movie = Movie.find(comment_params[:movie_id])
+      redirect_to(:controller => 'movies', :action => 'show', :id => @movie.id)
+    end
   end
-
-  def edit
+  
+  def comment_params
+    params.require(:comment).permit(:title, :name, :comment, :movie_id)
   end
-
-  def update
-  end
-
-  def destroy
-  end
+  
 end
