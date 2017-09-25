5a0d8f3bec72c34a52375af34bf55a6996d65fdb
arminderk <arminder.khinda@yahoo.com>
2017-09-25 16:02:56 -0700

    Added custom mailer, have to configure config/environments/development to get smtp to send emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  after_create :welcome_email
+    def welcome_email
+      UserMailer.welcome_email(self).deliver
+      redirect_to root_path, alert: "Thank You for registering"
+    end
 end
